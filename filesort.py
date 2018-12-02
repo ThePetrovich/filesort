8346f0e93d332c8387aa18596b65e76ad0674693
ThePetrovich <petroff.ap@gmail.com>
2018-12-02 18:19:04 +0900

    Bug fixes
    - Fixed lowercase/uppercase extension recognition
    - Fixed chdir() trowing a WindowsException if script is ran using cmd

--- filesort.py
+++ filesort.py
@@ -3,6 +3,7 @@ from os import listdir
 from os.path import isfile, join, isdir
 from os import path
 import shutil
+import time
 
 print("Hello!\nWelcome to FileSort 2.0.\nPlease set up folder associations in the following format:\n .extension folder\subfolder\etc \nExample: \n .zip Archives\Zip\nIf you want to put your folders somwhere, add 'dir folder\subfolder\etc' to the list.\nWhen you are done, type 'sort' to start the sorting process.")
 strIn = ""
@@ -18,6 +19,7 @@ while True:
         break
     temp = data.split(' ')
     associations[temp[0]] = temp[1]
+    time.sleep(0.01)
 
 for key, value in associations.items():
     print('{} {}'.format(key, value))
@@ -27,7 +29,8 @@ for key, value in associations.items():
 folderlist = list(folders)
 extlist = list(exts)
 
-os.chdir(os.path.dirname(__file__))
+absFilePath = os.path.abspath(__file__)
+os.chdir(os.path.dirname(absFilePath))
 cdir = os.getcwd()
 onlyfiles = [f for f in listdir(cdir) if isfile(join(cdir, f))]
 onlydirs = [f for f in listdir(cdir) if isdir(join(cdir, f))]
@@ -36,7 +39,7 @@ extensions = []
 print("\nFiles in directory:\n------------------------------------------------")
 for i in range(len(onlyfiles)):
     filename, file_extension = os.path.splitext(cdir+"\\"+onlyfiles[i])
-    extensions.append(file_extension)
+    extensions.append(file_extension.lower())
     print(onlyfiles[i])
 print("------------------------------------------------\n")
 
@@ -77,4 +80,5 @@ for i in range(len(folderlist)):
         os.rmdir(cdir+"\\"+folderlist[i])
 
 print("\nDone!")
-
+time.sleep(5)
+input('Press ENTER to exit')
